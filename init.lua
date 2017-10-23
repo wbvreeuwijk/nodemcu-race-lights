4323de77f4feaaec9a1396636e2663f394979d76
wbvreeuwijk <bas@reeuwijk.net>
2017-10-23 10:51:52 +0200

    Added Wifi Reset and improved Boot Bypass code

--- init.lua
+++ init.lua
@@ -1,11 +1,22 @@
-print("Traffic light firmware v2.0")
-node.setcpufreq(node.CPU160MHZ)
+print("Traffic light firmware v2.1")
+
+PIN_WIFI_RESET=6
+PIN_BOOT_BYPASS=4
+
+gpio.mode(PIN_WIFI_RESET,gpio.INT,gpio.PULLUP)
+gpio.trig(PIN_WIFI_RESET, "down", function(level, when)
+    print("Reset Wifi...")
+    wifi.sta.clearconfig()
+    tmr.delay(100)
+    node.restart()
+end)
+
 
 -- FAIL SAFE
-gpio.mode(4,gpio.INPUT)
-if(gpio.read(4) == 1) then 
+gpio.mode(PIN_BOOT_BYPASS,gpio.INPUT,gpio.PULLUP)
+if(gpio.read(PIN_BOOT_BYPASS) == 1) then
     print("Starting ...")
-    modules =  {"config.lua","globals.lua","lights.lc","wifi_setup.lua"} 
+    modules =  {"config.lua","globals.lua","lights.lua","wifi_setup.lua"} 
     
     for i in pairs(modules) do
         if(file.exists(modules[i])) then 
@@ -13,10 +24,11 @@ if(gpio.read(4) == 1) then
             dofile(modules[i]) 
         end
     end
+else
+    print("Bypassing init.lua")
 end
-
 -- in you init.lua:
-if adc.force_init_mode(adc.INIT_VDD33)
+if adc.force_init_mode(adc.INIT_ADC)
 then
   node.restart()
   return -- don't bother continuing, the restart is scheduled
